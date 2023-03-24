dbd873783641eef595bf1227ef9adae86de6b14a
katsunori waragai <katsunori.waragai@groove-x.com>
2023-03-24 15:04:13 +0900

    add scripts/jpg2mp4.py

---
+++ scripts/jpg2mp4.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import cv2
+
+if __name__ == "__main__":
+    """
+    image_dir のjpgファイルをmp4のファイルに変換する。
+    """
+    import argparse
+    parser = argparse.ArgumentParser(description="jpg2mp4")
+    parser.add_argument("dir", help="image_dir ")
+    args = parser.parse_args()
+
+    img_dir = Path(args.dir)
+    img_list = sorted(img_dir.glob("*.jpg"))
+    writer = None
+    for i, p in enumerate(img_list):
+        print(p)
+        cvimg = cv2.imread(str(p))
+        if cvimg is None:
+            continue
+
+        if writer is None:
+            out_movei_name = Path.home() / f"{img_dir.stem}.mp4"
+            apiPreference = 0
+            codec = cv2.VideoWriter_fourcc(*'mp4v')
+            fps = 15
+            size = (cvimg.shape[1], cvimg.shape[0])
+            print(f"{size=}")
+            writer = cv2.VideoWriter(str(out_movei_name), apiPreference, fourcc=codec, fps=fps, frameSize=size)
+
+
+        print("write", i, p)
+        writer.write(cvimg)
+
+    writer.release()
+    print(f"saved as {out_movei_name}")
